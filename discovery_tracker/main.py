d1938e9fe6ceff1386ba5b27f60ee26575161168
Kamil Breguła <kamil.bregula@polidea.com>
2020-03-15 23:21:16 +0100

    Fetch api definition without descriptions

--- discovery_tracker/main.py
+++ discovery_tracker/main.py
@@ -1,19 +1,19 @@
 import json
+from copy import deepcopy
 
 from googleapiclient import discovery as discovery_client
 from tqdm import tqdm
 
 NUM_RETRIES: int = 5
 
-def save_json(filename, tracked_service_list):
+
+def save_json(filename, content):
     with open(filename, 'w') as outfile:
-        json.dump(tracked_service_list, outfile, indent=" " * 4, sort_keys=True)
+        json.dump(content, outfile, indent=" " * 4, sort_keys=True)
 
 
 def run_main(args):
     discovery = discovery_client.build("discovery", "v1")
-
-    print(f"Fetching index")
 
     tracked_service_list = fetch_index(discovery)
 
@@ -29,18 +29,48 @@ def fetch_services(discovery, tracked_service_list):
         fetch_service(discovery, name, version)
 
 
+def remove_key(obj, key):
+    def _walk_recursive(_inner_obj):
+        if _inner_obj is None:
+            pass
+        if isinstance(_inner_obj, list):
+            for o in _inner_obj:
+                _walk_recursive(o)
+        if isinstance(_inner_obj, dict):
+            if key in _inner_obj:
+                del _inner_obj[key]
+            for o in _inner_obj.values():
+                _walk_recursive(o)
+
+    result = deepcopy(obj)
+    _walk_recursive(result)
+    return result
+
+
 def fetch_service(discovery, name: str, version: str):
-    api_description = (
+    api_definition = (
         discovery.apis()
-            .getRest(
+        .getRest(
             api=name,
             version=version
         ).execute(num_retries=NUM_RETRIES)
     )
-    save_json(f"gen_data/services/{name}_{version}.json", api_description)
+    if 'revision' in api_definition:
+        del api_definition['revision']
+
+    save_json(
+        filename=f"gen_data/services/{name}__{version}.json",
+        content=api_definition
+    )
+    api_definition_without_descriptions = remove_key(api_definition, "description")
+    save_json(
+        filename=f"gen_data/services_without_descriptions/{name}__{version}.json",
+        content=api_definition_without_descriptions
+    )
 
 
 def fetch_index(discovery):
+    print(f"Fetching index")
     api_service_response = discovery.apis().list().execute()
     api_service_list = [
         api_service
@@ -56,5 +86,4 @@ def fetch_index(discovery):
         for api_service in api_service_list
     ]
     tracked_service_list = sorted(tracked_service_list, key=lambda d: (d["name"], d["version"]))
-    save_json("gen_data/index.json", tracked_service_list)
     return tracked_service_list
